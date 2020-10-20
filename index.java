9822308ebf7bda8b3962ba1ace20aad43674a344
BenjiHarper1 <thebenjiharper@gmail.com>
2020-10-20 15:01:01 +0000

    Variables B project

--- index.java
+++ index.java
@@ -1,25 +1,34 @@
+
+
 void setup() {
-	size(500, 500);
+size(500, 500);
+background(255);
+//Draws first rectangle
+fill((int) random(1,255), (int) random(1,255), (int) random(1,255));
+rect((int) random(50,450), (int)random(50,450), (int)random(50,300), (int)random(50,300));
+
 }
 
 void draw() {
-// Step 1: Write code that draws the following screenshots 
-// with hard-coded values. (Feel free to use colors 
-// instead of grayscale.)
+//variables for the ellipse
+int eX =(int)random(mouseX + (int) random(-20,20), mouseX + random(-20,20));
+int eY =(int)random(mouseY + random(-20,20), mouseY + random(-20,20));
+fill((int) random(1,255), random(1,255), random(1,255));
 
-// Step 2: Replace all of the hard-coded numbers with 
-// variables.
+//draws ellipse
+ellipse(eX, eY, 10,10);
+}
 
-// Step 3: Write assignment operations in draw() that 
-// change the value of the variables.
-// For example, “variable1 = variable1 + 2;”. 
+void mousePressed() {
+//variables for the rectangle
+int w = (int)random(50,300);
+int z = (int)random(50,300);
+int x = (int)random(0,500);
+int y = (int)random(0,500);
+//Resets the screen and then draws the rectangle
+background(255);
+fill((int) random(1,255), (int) random(1,255), (int) random(1,255));
+rect(x, y, w, z);
 
-// Step 4: Make it so when the program runs the ball slides off the screen exiting at the point (500, 500)
-
-  background(255);
-  stroke(0);
-
-
-  
 
 }
